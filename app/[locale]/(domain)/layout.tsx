895292baafa2ec7d3d05d7ea5819e5effe01b756
Vitor Valentim <vitorvalentin840@gmail.com>
2025-12-21 04:11:41 -0300

    feat(header): implement header component and integrate into domain layout

--- app/[locale]/(domain)/layout.tsx
+++ app/[locale]/(domain)/layout.tsx
@@ -1,3 +1,4 @@
+import { Header } from "@/app/_components/header";
 import { ReactNode } from "react";
 
 interface DomainLayoutProps {
@@ -6,9 +7,30 @@ interface DomainLayoutProps {
 
 export default function DomainLayout({ children }: DomainLayoutProps) {
   return (
-    <>
-      <p>teste DomainLayout</p>
+    <div className="dark theme-domain min-h-screen">
+      <Header
+        title={{
+          text: "Multi",
+          text2: "Shop",
+          href: "/",
+          type: "icon",
+          icon: "shoppingBag",
+        }}
+        navLinks={[
+          { href: "/", label: "Home" },
+          { href: "/sobre", label: "Sobre" },
+          { href: "/funcionalidades", label: "Funcionalidades" },
+          { href: "/como-funciona", label: "Como Funciona" },
+          { href: "/planos", label: "Planos" },
+          { href: "/contato", label: "Contato" },
+        ]}
+        button={[
+          { variant: "secondary", size: "sm", title: "Entrar" },
+          { variant: "default", size: "sm", title: "Criar Minha Loja" },
+        ]}
+      />
+
       {children}
-    </>
+    </div>
   );
 }
--- app/[locale]/(domain)/page.tsx
+++ app/[locale]/(domain)/page.tsx
@@ -1,5 +1,3 @@
-import { ModeToggle } from "@/app/_components/mode-toggle";
-import { LanguageSwitcher } from "@/app/_components/language-switcher";
 import { Locale } from "@/app/_lib/i18n/config";
 import { tServer } from "@/app/_lib/i18n/server";
 
@@ -15,10 +13,7 @@ export default async function HomeDomain({ params }: HomeDomainProps) {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <ModeToggle />
-        <p>teste HomeDomain</p>
         <p>{tServer("tenant.notFound", locale as Locale)}</p>
-        <LanguageSwitcher />
       </main>
     </div>
   );

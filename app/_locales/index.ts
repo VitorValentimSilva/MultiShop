96e64e937bf2949914c85a5932ff34a81890b77e
Vitor Valentim <vitorvalentin840@gmail.com>
2025-12-23 22:36:00 -0300

    feat(i18n): add translations for new error codes and update config

---
+++ app/_locales/index.ts
@@ -0,0 +1,75 @@
+import ptBRTenant from "@/app/_locales/tenant/pt-BR/tenant.json";
+import ptBRTenantError from "@/app/_locales/tenant/pt-BR/tenant-error.json";
+import enUSTenant from "@/app/_locales/tenant/en-US/tenant.json";
+import enUSTenantError from "@/app/_locales/tenant/en-US/tenant-error.json";
+import ptBRSession from "@/app/_locales/session/pt-BR/session.json";
+import ptBRSessionError from "@/app/_locales/session/pt-BR/session-error.json";
+import enUSSession from "@/app/_locales/session/en-US/session.json";
+import enUSSessionError from "@/app/_locales/session/en-US/session-error.json";
+import ptBRAccount from "@/app/_locales/account/pt-BR/account.json";
+import ptBRAccountError from "@/app/_locales/account/pt-BR/account-error.json";
+import enUSAccount from "@/app/_locales/account/en-US/account.json";
+import enUSAccountError from "@/app/_locales/account/en-US/account-error.json";
+import ptBRUser from "@/app/_locales/user/pt-BR/user.json";
+import ptBRUserError from "@/app/_locales/user/pt-BR/user-error.json";
+import enUSUser from "@/app/_locales/user/en-US/user.json";
+import enUSUserError from "@/app/_locales/user/en-US/user-error.json";
+import ptBTRole from "@/app/_locales/role/pt-BR/role.json";
+import ptBTRoleError from "@/app/_locales/role/pt-BR/role-error.json";
+import enUSRole from "@/app/_locales/role/en-US/role.json";
+import enUSRoleError from "@/app/_locales/role/en-US/role-error.json";
+import ptBRPermission from "@/app/_locales/permission/pt-BR/permission.json";
+import ptBRPermissionError from "@/app/_locales/permission/pt-BR/permission-error.json";
+import enUSPermission from "@/app/_locales/permission/en-US/permission.json";
+import enUSPermissionError from "@/app/_locales/permission/en-US/permission-error.json";
+import ptBRUserRole from "@/app/_locales/user-role/pt-BR/user-role.json";
+import ptBRUserRoleError from "@/app/_locales/user-role/pt-BR/user-role-error.json";
+import enUSUserRole from "@/app/_locales/user-role/en-US/user-role.json";
+import enUSUserRoleError from "@/app/_locales/user-role/en-US/user-role-error.json";
+import ptBRRolePermission from "@/app/_locales/role-permission/pt-BR/role-permission.json";
+import ptBRRolePermissionError from "@/app/_locales/role-permission/pt-BR/role-permission-error.json";
+import enUSRolePermission from "@/app/_locales/role-permission/en-US/role-permission.json";
+import enUSRolePermissionError from "@/app/_locales/role-permission/en-US/role-permission-error.json";
+import ptBRVerificationToken from "@/app/_locales/verification-token/pt-BR/verification-token.json";
+import ptBRVerificationTokenError from "@/app/_locales/verification-token/pt-BR/verification-token-error.json";
+import enUSVerificationToken from "@/app/_locales/verification-token/en-US/verification-token.json";
+import enUSVerificationTokenError from "@/app/_locales/verification-token/en-US/verification-token-error.json";
+
+export {
+  ptBRTenant,
+  ptBRTenantError,
+  enUSTenant,
+  enUSTenantError,
+  ptBRSession,
+  ptBRSessionError,
+  enUSSession,
+  enUSSessionError,
+  ptBRAccount,
+  ptBRAccountError,
+  enUSAccount,
+  enUSAccountError,
+  ptBRUser,
+  ptBRUserError,
+  enUSUser,
+  enUSUserError,
+  ptBTRole,
+  ptBTRoleError,
+  enUSRole,
+  enUSRoleError,
+  ptBRPermission,
+  ptBRPermissionError,
+  enUSPermission,
+  enUSPermissionError,
+  ptBRUserRole,
+  ptBRUserRoleError,
+  enUSUserRole,
+  enUSUserRoleError,
+  ptBRRolePermission,
+  ptBRRolePermissionError,
+  enUSRolePermission,
+  enUSRolePermissionError,
+  ptBRVerificationToken,
+  ptBRVerificationTokenError,
+  enUSVerificationToken,
+  enUSVerificationTokenError,
+};

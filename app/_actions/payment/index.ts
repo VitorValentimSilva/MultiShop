7d2141fe5d878ab5f846b859dddfc3ea22d4cbb4
Vitor Valentim <vitorvalentin840@gmail.com>
2025-12-26 18:30:37 -0300

    feat(stripe): implement initial payment infrastructure and configurations

---
+++ app/_actions/payment/index.ts
@@ -0,0 +1,1 @@
+export * from "@/app/_actions/payment/checkout";
---
+++ app/api/webhook/stripe/route.ts
@@ -0,0 +1,73 @@
+import { prisma, stripe } from "@/app/_lib";
+import { headers } from "next/headers";
+import Stripe from "stripe";
+
+export async function POST(req: Request) {
+  const body = await req.text();
+  const signature = (await headers()).get("Stripe-Signature") as string;
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET!,
+    );
+  } catch (err) {
+    console.log("⚠️  Webhook signature verification failed.", err);
+    return new Response(`Webhook Error`, { status: 400 });
+  }
+
+  const session = event.data.object as Stripe.Checkout.Session;
+
+  if (event.type === "checkout.session.completed") {
+    const subscription = await stripe.subscriptions.retrieve(
+      session.subscription as string,
+    );
+
+    if (!session.metadata?.tenantId) {
+      return new Response("Missing tenantId in metadata", { status: 400 });
+    }
+
+    const plan = await prisma.plan.findFirst({
+      where: { stripePriceId: subscription.items.data[0].price.id },
+    });
+
+    if (!plan) {
+      return new Response("Plan not found", { status: 400 });
+    }
+
+    await prisma.tenant.update({
+      where: { id: session.metadata.tenantId },
+      data: {
+        stripeSubscriptionId: subscription.id,
+        stripeCustomerId:
+          typeof session.customer === "string"
+            ? session.customer
+            : session.customer?.id,
+        subscriptionStatus: subscription.status,
+        isActive: true,
+        plan: {
+          connect: { id: plan.id },
+        },
+      },
+    });
+  }
+
+  if (
+    event.type === "customer.subscription.deleted" ||
+    event.type === "customer.subscription.updated"
+  ) {
+    const subscription = event.data.object;
+    await prisma.tenant.update({
+      where: { stripeSubscriptionId: subscription.id },
+      data: {
+        subscriptionStatus: subscription.status,
+        isActive: subscription.status === "active",
+      },
+    });
+  }
+
+  return new Response(null, { status: 200 });
+}

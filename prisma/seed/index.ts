645e66c048cd96db1ac0f79d5cead6269f2ea73c
Vitor Valentim <vitorvalentin840@gmail.com>
2025-12-26 18:29:37 -0300

    feat(prisma): update tenant and plan entities with new schema and seeds

--- prisma/seed/index.ts
+++ prisma/seed/index.ts
@@ -2,15 +2,15 @@ import { seedPermissions } from "@/prisma/seed/permissions.seed";
 import { seedTenants } from "@/prisma/seed/tenants.seed";
 import { seedRoles } from "@/prisma/seed/roles.seed";
 import { seedUsers } from "@/prisma/seed/users.seed";
-import { seedDomain } from "./domain.seed";
+import { seedDomain } from "@/prisma/seed/domain.seed";
 
 export async function runSeeds() {
   await seedPermissions();
+
+  await seedDomain();
 
   const tenant = await seedTenants();
   const adminRole = await seedRoles(tenant.id);
 
   await seedUsers(tenant.id, adminRole.id);
-
-  await seedDomain();
 }
--- prisma/seed/tenants.seed.ts
+++ prisma/seed/tenants.seed.ts
@@ -1,13 +1,21 @@
 import { prisma } from "@/app/_lib/prisma";
 
 export async function seedTenants() {
+  const starterPlan = await prisma.plan.findUnique({
+    where: { key: "starter" },
+  });
+
   const tenant = await prisma.tenant.upsert({
     where: { slug: "default" },
-    update: {},
+    update: {
+      planId: starterPlan?.id,
+    },
     create: {
       slug: "default",
       name: "Tenant Default",
       isActive: true,
+      subscriptionStatus: "active",
+      planId: starterPlan?.id,
     },
   });
 

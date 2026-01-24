bc2b6994775cf7afd5fb8f0c32e1c1073d748b16
Vitor Valentim da Silva Ferreira <vitorvalentin840@gmail.com>
2026-01-24 18:14:48 -0300

    feat: create index file to export utility modules

---
+++ src/core/utils/index.ts
@@ -0,0 +1,5 @@
+export * from "@/core/utils/common";
+export * from "@/core/utils/date";
+export * from "@/core/utils/filter";
+export * from "@/core/utils/functional";
+export * from "@/core/utils/locale";

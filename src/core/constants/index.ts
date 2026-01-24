c2c5f533599d95d66ac379be525137bd34400d67
Vitor Valentim da Silva Ferreira <vitorvalentin840@gmail.com>
2026-01-24 20:56:25 -0300

    feat: export i18n constants in the main constants index

--- src/core/constants/index.ts
+++ src/core/constants/index.ts
@@ -1,4 +1,5 @@
 export * from "@/core/constants/date.constants";
 export * from "@/core/constants/filter.constants";
+export * from "@/core/constants/i18n.constants";
 export * from "@/core/constants/locale.constants";
 export * from "@/core/constants/pagination.constants";

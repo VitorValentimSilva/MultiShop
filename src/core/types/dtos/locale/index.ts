3c586f2dc8ddacce6f56ee011e2b7674329f2071
Vitor Valentim da Silva Ferreira <vitorvalentin840@gmail.com>
2026-01-24 23:04:46 -0300

    feat: export middleware.dtos from locale index

--- src/core/types/dtos/locale/index.ts
+++ src/core/types/dtos/locale/index.ts
@@ -1,3 +1,4 @@
 export * from "@/core/types/dtos/locale/format.dtos";
+export * from "@/core/types/dtos/locale/middleware.dtos";
 export * from "@/core/types/dtos/locale/request.dtos";
 export * from "@/core/types/dtos/locale/translation.dtos";

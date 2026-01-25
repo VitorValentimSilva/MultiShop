f976d4469aeeeabe08a5df4302f9a6d0379b3570
Vitor Valentim da Silva Ferreira <vitorvalentin840@gmail.com>
2026-01-25 18:54:32 -0300

    refactor: remove unused language switcher components

--- src/features/shared/components/language-switcher/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "@/features/shared/components/language-switcher/language-switcher-base";
-export * from "@/features/shared/components/language-switcher/language-switcher-standalone";
-export * from "@/features/shared/components/language-switcher/language-switcher";

560cd17a4990d316fc8303212d75f1f42c8bbae0
Vitor Valentim da Silva Ferreira <vitorvalentin840@gmail.com>
2026-01-24 22:22:31 -0300

    feat: create index file for i18n exports

---
+++ src/core/infrastructure/i18n/index.ts
@@ -0,0 +1,5 @@
+export * from "@/core/infrastructure/i18n/client";
+export * from "@/core/infrastructure/i18n/hooks";
+export * from "@/core/infrastructure/i18n/entity-translator";
+export * from "@/core/infrastructure/i18n/i18n-server";
+export * from "@/core/infrastructure/i18n/i18n.config";

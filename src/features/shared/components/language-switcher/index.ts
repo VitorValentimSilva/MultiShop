3cb70fe2084c288d6815a4931c90d2979646d160
Vitor Valentim da Silva Ferreira <vitorvalentin840@gmail.com>
2026-01-25 17:38:31 -0300

    feat: add index file for language-switcher components

---
+++ src/features/shared/components/language-switcher/index.ts
@@ -0,0 +1,3 @@
+export * from "@/features/shared/components/language-switcher/language-switcher-base";
+export * from "@/features/shared/components/language-switcher/language-switcher-standalone";
+export * from "@/features/shared/components/language-switcher/language-switcher";

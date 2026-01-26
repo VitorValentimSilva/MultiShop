3e186b659308a832092705089f6b8cee02c97425
Vitor Valentim da Silva Ferreira <vitorvalentin840@gmail.com>
2026-01-25 22:02:07 -0300

    feat: add i18n client, hooks, and configuration exports

---
+++ src/core/infrastructure/i18n/client/index.ts
@@ -0,0 +1,3 @@
+export * from "@/core/infrastructure/i18n/client/i18n-client.utils";
+export * from "@/core/infrastructure/i18n/client/i18n-context";
+export * from "@/core/infrastructure/i18n/client/i18n-provider";

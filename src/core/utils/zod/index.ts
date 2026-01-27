e8fbe2f06ec8791320cf69940d5d30fa25ec7264
Vitor Valentim da Silva Ferreira <vitorvalentin840@gmail.com>
2026-01-27 20:00:04 -0300

    feat: remove deprecated Zod schemas and update exports

--- src/core/utils/zod/index.ts
+++ src/core/utils/zod/index.ts
@@ -1,2 +1,1 @@
-export * from "@/core/utils/zod/schemas";
 export * from "@/core/utils/zod/zod.utils";
--- src/core/utils/zod/schemas/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "@/core/utils/zod/schemas/common.schemas";
-export * from "@/core/utils/zod/schemas/date.schemas";
-export * from "@/core/utils/zod/schemas/entity.schemas";
-export * from "@/core/utils/zod/schemas/locale.schemas";
-export * from "@/core/utils/zod/schemas/nanoid.schemas";
-export * from "@/core/utils/zod/schemas/pagination.schemas";
